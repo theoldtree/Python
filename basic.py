8dbb01fe0d0a8d8bbbf620f81e5299629d86096e
theoldtree <yjeongmu@gmail.com>
2022-07-11 17:03:53 +0900

    파이썬 기초 4일차

--- basic.py
+++ basic.py
@@ -16,6 +16,12 @@ a = {1,2,3,4,5} # 집합 자료 형 -> 중복 허용 x , 순서가 없는 나열
 greet = "hello" # 문자열은 튜플과 성질이 비슷하다
 list(greet) # ['h','e','l','l','o']
 
+#리스트에서 쓰는 함수
+list = [10,11,12]
+list.append(13)
+index = 0 
+del(list[index])
+
 #숫자의 연산
 c = 3/2 # 정수/정수의 연산 결과가 실수가 됨 ㅋㅋ
 print(type(c))
@@ -27,7 +33,7 @@ d = 3 ** 4 # 거듭제곱의 표현
 print(d) # 81
 
 #형 변환
-float(), int(), tuple(), list(), str()
+# float(object), int(object), tuple(object), list(object), str(object)
 
 #문자열의 분리와 합치기
 friends_list = "홍춘향 이몽룡 이도령"
@@ -75,7 +81,30 @@ for i in range(1,11,2): # 1부터 10 까지 2 씩 띄워가며 덧셈
     sum += i
 print(sum)
 
+#반복문을 이용하여 리스트 만들기
+v=[]
+for k in range(1,11):
+    v.append(k)
+print(v)
+
 # 함수의 정의
 def sum_of_list(a,b,c,d):
     return a+b+c+d
 print(sum_of_list(5,6,7,8))
+
+# 사전형 - dict형
+numbers = {"one":1, "two":2, "three":5}
+numbers["four"] = 4 # 새로운 항목의 추가
+numbers["three"] = 3
+print(tuple(numbers.keys()))
+print(tuple(numbers.values()))
+for k in numbers:
+    print(k,numbers[k])
+
+# 사전 형을 이용한 빈도계산
+a = (3,4,1,2,3,2,4)
+h={}
+for k in a:
+    if k in h:h[k] += 1
+    else: h[k] = 1
+print(h)
